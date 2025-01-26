da8df19449ed8fe54edf91943d410ed59938b4c1
Adam <62399795+Adamme-le-codeur-fou@users.noreply.github.com>
2025-01-26 16:35:30 +0100

    ezzzzzzzzzzzzzzzz

--- Assets/Scripts/UI/Menu/Cell.cs
+++ Assets/Scripts/UI/Menu/Cell.cs
@@ -1,19 +1,25 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.Menu
 {
     public class Cell : MonoBehaviour
     {
-
+        
         [SerializeField] private GameObject outline;
 
-        private TextMeshProUGUI inputField; 
+        [SerializeField] private List<Sprite> outlinesPlayerList;
+
+        private TextMeshProUGUI inputField;
+        private Image oultineImage;
     
         private void Awake()
         {
             SetOutlineActive(false);
             inputField = GetComponentInChildren<TextMeshProUGUI>();
+            oultineImage = outline.GetComponentInChildren<Image>();
         }
 
         public void SetOutlineActive(bool active)
@@ -24,6 +30,12 @@ namespace UI.Menu
         public void SetText(string text)
         {
             inputField.text = text;
+        }
+
+        public void SetOutlineColor(int playerID)
+        {
+            if (playerID < outlinesPlayerList.Count)
+                oultineImage.sprite = outlinesPlayerList[playerID];
         }
     }
 }
